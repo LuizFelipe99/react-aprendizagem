e8f2cbaff8de86639d452440b39191262d6c4017
Luiz Felipe Tomaz <luizfelipetomaz30@gmail.com>
2022-12-29 07:36:54 -0300

    segundo commit

---
+++ src/compoennts/Luiz/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './luiz.css';
+
+const Luiz = () =>{
+  return (
+    <div className='container'>
+      <h1>LUIZ LINDAAAAO</h1>
+      <div class="heart">
+    </div>
+    </div>
+  );
+}
+
+export default Luiz;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './compoennts/Home';
 import Sobre  from './compoennts/Sobre';
 import Elvis from './compoennts/Elvis';
+import Luiz from './compoennts/Luiz';
 
 function Rotas() {
   return (
@@ -11,6 +12,7 @@ function Rotas() {
         <Route path="/" element={<Home/>} />
         <Route path="/sobre" element={<Sobre/>} />
         <Route path="/elvis" element={<Elvis/>} />
+        <Route path="/luiz" element={<Luiz/>} />
       </Routes>
     </div>
   );
